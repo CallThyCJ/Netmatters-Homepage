0b33dbe92fa45c5281bf686b2b0b7294da5ae0d4
CallThyCJ <135438020+CallThyCJ@users.noreply.github.com>
2024-08-27 11:48:21 +0100

    Button Border progress
    Managed to correct the spacing of the border for the banner buttons. Trying to now get the border to appear with the images using JS. Currently not working

--- JS/bannerSlider.js
+++ JS/bannerSlider.js
@@ -2,6 +2,7 @@ const bannerImages = document.querySelectorAll(".bannerImage");
 let bannerImageIndex = 0;
 let intervalID = null;
 const bannerInfo = document.querySelectorAll(".bannerInfo");
+const buttons = document.querySelectorAll(".sliderButtonContainer button");
 
 
 document.addEventListener("DOMContentLoaded", initializeImage);
@@ -9,7 +10,9 @@ document.addEventListener("DOMContentLoaded", initializeImage);
 function initializeImage() {
     bannerImages[bannerImageIndex].classList.add("displayItem");
     bannerInfo[bannerImageIndex].style.display = "flex";
+    buttons[bannerImageIndex].classList.add("active");
     intervalID = setInterval(nextImage, 5000);
+    
 
 }
 
@@ -26,9 +29,13 @@ function showImages(index) {
     bannerInfo.forEach(info => {
         info.style.display = "none";
     });
+    buttons.forEach(button => {
+        button.classList.remove("active");
+    });
     
     bannerImages[bannerImageIndex].classList.add("displayItem");
     bannerInfo[bannerImageIndex].style.display = "flex";
+    buttons[bannerImageIndex].classList.add("active");
 
 }
 
