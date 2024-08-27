8de816465f4ba9418449a17b83c9c41e916677aa
CallThyCJ <135438020+CallThyCJ@users.noreply.github.com>
2024-08-27 17:55:55 +0100

    burger menu button transition
    added JS functionality to transition burger menu icon to an X when the button is clicked. Then vice versa. Need to make the icon also spin when this happens next.

--- JS/sideMenu.js
+++ JS/sideMenu.js
@@ -1,16 +1,27 @@
 const button = document.querySelector("#burgerMenuButton");
 const sideMenu = document.querySelector(".sideMenuContainer");
 const overlay = document.querySelector("#overlay");
+const menuIcon = document.querySelector("#menuIcon");
 let sideMenuOpen = false;
 
 button.addEventListener("click", function() {
     if (sideMenuOpen === false) {
+        menuIcon.style.opacity = "0";
+        setTimeout(() => {
+            menuIcon.classList.replace("fa-bars", "fa-x");
+            menuIcon.style.opacity = "1";
+        }, 200);
         sideMenu.classList.add("sideMenuShow");
         sideMenu.classList.remove("sideMenuHide");
         overlay.style.display = "block";
         setTimeout(() => overlay.style.opacity = "1", 10);
         sideMenuOpen = true;
     } else {
+        menuIcon.style.opacity = "0";
+        setTimeout(() => {
+            menuIcon.classList.replace("fa-x", "fa-bars");
+            menuIcon.style.opacity = "1";
+        }, 200);
         sideMenu.classList.add("sideMenuHide");
         sideMenu.classList.remove("sideMenuShow");
         overlay.style.opacity = "0";
@@ -21,6 +32,11 @@ button.addEventListener("click", function() {
 
 overlay.addEventListener("click", function () {
     if (sideMenuOpen === true) {
+        menuIcon.style.opacity = "0";
+        setTimeout(() => {
+            menuIcon.classList.replace("fa-x", "fa-bars");
+            menuIcon.style.opacity = "1";
+        }, 200);
         sideMenu.classList.add("sideMenuHide");
         sideMenu.classList.remove("sideMenuShow");
         overlay.style.opacity = "0";
