bea3630a5ad794992f9c2437dd0fdf5279963c0f
CallThyCJ <135438020+CallThyCJ@users.noreply.github.com>
2024-08-29 15:53:44 +0100

    Fixed Sticky Header
    fixed sticky header so both header and nav bar stick when scrolling up.

--- JS/stickyNavBar.js
+++ JS/stickyNavBar.js
@@ -1,27 +1,27 @@
 const header = document.getElementById("mainHeader");
 const navBar = document.querySelector(".mainNavBar");
+const navBarPos = document.querySelector(".mainNavBarPosition");
 let lastScroll = 0;
+const scrollLimit = 217;
 
 window.addEventListener("scroll", () => {
     const currentScroll = window.scrollY;
 
     if (currentScroll <= 0) {
-        header.classList.remove("scrollUp");
-        navBar.classList.remove("scrollUp")
-    }
-
-    if (currentScroll > lastScroll && !header.classList.contains("scrollDown")) {
+        header.classList.remove("scrollUp", "scrollDown");
+        navBar.classList.remove("scrollUpNavBar", "scrollDownNavBar");
+        navBarPos.classList.add("mainNavBarPosition");
+    } else if (currentScroll > scrollLimit && currentScroll > lastScroll && !header.classList.contains("scrollDown")) {
+        navBarPos.classList.remove("mainNavBarPosition");
         header.classList.remove("scrollUp");
         header.classList.add("scrollDown");
-        navBar.classList.remove("scrollUp");
-        navBar.classList.add("scrollDown");
-    }
-
-    if (currentScroll < lastScroll && header.classList.contains("scrollDown")) {
+        navBar.classList.remove("scrollUpNavBar");
+        navBar.classList.add("scrollDownNavBar");
+    } else if (currentScroll < lastScroll && header.classList.contains("scrollDown")) {
         header.classList.remove("scrollDown");
         header.classList.add("scrollUp");
-        navBar.classList.remove("scrollDown");
-        navBar.classList.add("scrollUp");
+        navBar.classList.remove("scrollDownNavBar");
+        navBar.classList.add("scrollUpNavBar");
     }    
 
     lastScroll = currentScroll;
